237f55a78ccdfc6d921042ab4ff321a3fe9fadbe
Pawel Grzybek <pawelgrzybek@users.noreply.github.com>
2021-07-09 17:51:05 +0100

    Add infrastrucutre tests

--- test/jamstack-cdk-comments.test.ts
+++ test/jamstack-cdk-comments.test.ts
@@ -1,37 +1,36 @@
-// import {
-//   expect as expectCDK,
-//   haveResource,
-//   haveResourceLike,
-//   countResources,
-// } from "@aws-cdk/assert";
-// import * as cdk from "@aws-cdk/core";
-// import * as Comments from "../lib/pawelgrzybek-comments-stack";
+import {
+  expect as expectCDK,
+  haveResource,
+  countResources,
+} from "@aws-cdk/assert";
+import * as cdk from "@aws-cdk/core";
+import * as Comments from "../lib/pawelgrzybek-comments-stack";
 
-// describe("JAMstack CDK comments Stack", () => {
-//   const app = new cdk.App();
-//   const stack = new Comments.Stack(app, "MyTestStack", {
-//     allowOrigins: ["https://pawelgrzybek.com"],
-//   });
+describe("JAMstack CDK comments Stack", () => {
+  const app = new cdk.App();
+  const stack = new Comments.Stack(app, "MyTestStack", {
+    allowOrigins: ["https://pawelgrzybek.com"],
+  });
 
-//   it("creates DynamoDB", () => {
-//     expectCDK(stack).to(haveResource("AWS::DynamoDB::Table"));
-//   });
+  it("creates DynamoDB", () => {
+    expectCDK(stack).to(haveResource("AWS::DynamoDB::Table"));
+  });
 
-//   it("creates Lambdas", () => {
-//     expectCDK(stack).to(countResources("AWS::Lambda::Function", 3));
-//     expectCDK(stack).to(
-//       haveResource("AWS::Lambda::Function", {
-//         Handler: "index.handler",
-//         Runtime: "nodejs14.x",
-//       })
-//     );
-//   });
+  it("creates S3 Bucket", () => {
+    expectCDK(stack).to(haveResource("AWS::S3::Bucket"));
+  });
 
-//   it("creates API Gateway", () => {
-//     expectCDK(stack).to(
-//       haveResourceLike("AWS::ApiGateway::RestApi", {
-//         Name: "CommentsApi",
-//       })
-//     );
-//   });
-// });
+  it("creates Rest Api", () => {
+    expectCDK(stack).to(haveResource("AWS::ApiGateway::RestApi"));
+  });
+
+  it("creates Lambdas", () => {
+    expectCDK(stack).to(countResources("AWS::Lambda::Function", 3));
+    expectCDK(stack).to(
+      haveResource("AWS::Lambda::Function", {
+        Handler: "index.handler",
+        Runtime: "nodejs14.x",
+      })
+    );
+  });
+});
